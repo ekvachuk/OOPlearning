e46134e1dd01726aba123946e4487d5e4bf5c6a5
ekvachuk <jekvach@ukr.net>
2019-05-28 21:39:50 +0300

    Initial commit

---
+++ src/org/itstep/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package org.itstep;
+
+import org.itstep.model.Circle;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(10);
+        System.out.println("Circle has radius"+ circle.getRadius());
+        System.out.println("Circle has area"+ circle.getArea());
+    }
+}
---
+++ src/org/itstep/model/Triangle.java
@@ -0,0 +1,28 @@
+package org.itstep.model;
+
+public class Triangle extends AbstractShape {
+
+    private double base;
+    private double height;
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return base*height/2;
+    }
+}
