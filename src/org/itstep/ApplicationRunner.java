4e29ebaf85578e5b36a77fe7628229805e5effe4
ekvachuk <jekvach@ukr.net>
2019-06-17 21:03:42 +0300

    Add interface

--- src/org/itstep/ApplicationRunner.java
+++ src/org/itstep/ApplicationRunner.java
@@ -1,13 +1,44 @@
 package org.itstep;
 
 import org.itstep.model.Circle;
+import org.itstep.model.Shape;
+import org.itstep.model.Square;
+import org.itstep.model.Triangle;
+
+import java.util.*;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        Circle circle = new Circle();
-        circle.setRadius(10);
-        System.out.println("Circle has radius"+ circle.getRadius());
-        System.out.println("Circle has area"+ circle.getArea());
+        // Circle circle = new Circle();
+        //circle.setRadius(10);
+        //System.out.println("Circle has radius"+ circle.getRadius());
+        //System.out.println("Circle has area"+ circle.getArea());
+
+        //String str = "str1";
+        //String [] strings = new String[3];
+        //strings [0] = "str";
+        //strings [1] = "str2";
+        //strings [2] = "str3";
+
+        //System.out.println(strings [0]);
+
+        Circle circle = new Circle(15);
+
+        Triangle triangle = new Triangle(10, 8);
+
+        Square square = new Square(18);
+
+        List<Shape> shapeList = new ArrayList<>();
+        shapeList.add(circle);
+        shapeList.add(triangle);
+        shapeList.add(square);
+        shapeList.add(circle);
+
+        System.out.println("Size of list is " + shapeList.size());
+
+        Set<Shape> shapeSet = new HashSet<>(shapeList);
+
+        System.out.println("Size of set is " + shapeSet.size());
     }
 }
