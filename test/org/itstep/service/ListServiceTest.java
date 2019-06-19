6b6d8f61d8efb02ead88ae6b3731170653704066
ekvachuk <jekvach@ukr.net>
2019-06-19 21:14:39 +0300

    Add ListServiceTest

---
+++ test/org/itstep/service/ListServiceTest.java
@@ -0,0 +1,39 @@
+package org.itstep.service;
+
+import org.itstep.model.Circle;
+import org.itstep.model.Shape;
+import org.itstep.model.Square;
+import org.itstep.model.Triangle;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListServiceTest {
+
+    @Test
+    void getMaxShape() {
+        Circle circle = new Circle(15);
+        Triangle triangle = new Triangle(10, 8);
+        Square square = new Square(18);
+
+        List<Shape> shapeList = new ArrayList<>();
+        shapeList.add(circle);
+        shapeList.add(triangle);
+        shapeList.add(square);
+        shapeList.add(circle);
+
+        assertNotNull(shapeList);
+
+        Shape maxShape = ListService.getMaxShape(shapeList);
+
+        assertNotNull(maxShape);
+        assertEquals(maxShape.getClass().getSimpleName(), "Square");
+        assertEquals(maxShape.getArea(),100);
+
+        Shape nullShape = ListService.getMaxShape(new ArrayList<>());
+        assertNotNull(nullShape);
+    }
+}
